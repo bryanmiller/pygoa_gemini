2030a99a47552c7a9b15b86dd7fcbfcedf20e1b8
Bryan Miller <millerwbryan@gmail.com>
2019-09-26 23:17:45 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pygoa',
+    version='0.1.0',
+    author="Bryan Miller",
+    author_email="millerwbryan@gmail.com",
+    description="A simple python library for working with the Gemini Observatory Archive APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bryanmiller/pygoa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    keywords=['astronomy', 'astrophysics', 'science', 'fits', 'observatory', 'gemini', 'archive'],
+    install_requires=[
+        'requests',
+    ]
+)
